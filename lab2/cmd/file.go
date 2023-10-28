97aee9246b91d7db66c659fcec8148ffc37a4494
Спиридонов Владимир <vovaspiridonov92@gmail.com>
2023-10-29 02:36:15 +0300

    add quickSort

--- lab2/cmd/file.go
+++ lab2/cmd/file.go
@@ -1,8 +1,33 @@
 package main
 
-import "sort"
-
 func findKthLargest(nums []int, k int) int {
-	sort.IntSlice.Sort(nums)
+	nums = quickSort(nums, 0, len(nums)-1)
 	return nums[len(nums)-k]
+}
+
+func partition(arr []int, low int, high int) ([]int, int) {
+	pivot := arr[high]
+	i := low
+	for j := low; j < high; j++ {
+		if arr[j] < pivot {
+			tmp := arr[i]
+			arr[i] = arr[j]
+			arr[j] = tmp
+			i++
+		}
+	}
+	tmp := arr[i]
+	arr[i] = arr[high]
+	arr[high] = tmp
+	return arr, i
+}
+
+func quickSort(arr []int, low int, high int) []int {
+	if low < high {
+		var p int
+		arr, p = partition(arr, low, high)
+		arr = quickSort(arr, low, p-1)
+		arr = quickSort(arr, p+1, high)
+	}
+	return arr
 }
